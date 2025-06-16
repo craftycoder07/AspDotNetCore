4c5624a829f46b9d7bbf59be6ec49ed8e6fb831d
craftycoder07 <22353899+craftycoder07@users.noreply.github.com>
2025-06-15 22:58:25 -0400

    Demo for minimal API application

--- Handling Requestes With Middleware Pipeline/WebApplication1/WebApplication1/Program.cs
+++ Handling Requestes With Middleware Pipeline/WebApplication1/WebApplication1/Program.cs
@@ -3,6 +3,23 @@ using Microsoft.AspNetCore.Diagnostics;
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    /*
+     * In development, you want to give details of the error in order to fix them.
+     * This middleware gets automatically added in development environment.
+     */
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    /*
+     * In production, you might want to divert user to more generic page.
+     * This will rerun the middleware pipeline with PATH=/Error and execute that endpoint.
+     */
+    app.UseExceptionHandler("/Error");
+}
+
 /*
  * Middleware in ASP.NET is nothing but a c# class that can handle HTTP request.
  * .NET convention is to use Use* extension methods to add middlewares to the pipeline.
@@ -20,5 +37,8 @@ app.UseWelcomePage("/");
  * If file is available, it will be served else client will get 404(not found) error
  */
 app.UseStaticFiles();
+
+app.MapGet("/Error", () => "Error occurred.");
+app.MapGet("/HelloWorld", () => "Hello World!.");
 
 app.Run();
