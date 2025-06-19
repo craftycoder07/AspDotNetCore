a47ef4bc4acbc2f503eaa1e81f63daff0e7c84e2
craftycoder07 <22353899+craftycoder07@users.noreply.github.com>
2025-06-18 21:43:55 -0400

    Demo on parameterized routes

--- 3. JSON API with Minimal API/Program.cs
+++ 3. JSON API with Minimal API/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Parameterized routes
+app.MapGet("/firstname/{firstName}", (string firstName) => $"Hello {firstName}!");
+
+//Nullable parameterized routes
+app.MapGet("/id/{id?}", (int? id) => id == null ? "You did not pass any id" : $"Your passed: {id}");
 
 app.Run();
