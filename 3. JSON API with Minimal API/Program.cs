7b44c5a275a5a9b57fb37464538afcd69c0e59a9
craftycoder07 <22353899+craftycoder07@users.noreply.github.com>
2025-06-25 23:04:19 -0400

    Demo on how to use IResult Types

--- 3. JSON API with Minimal API/Program.cs
+++ 3. JSON API with Minimal API/Program.cs
@@ -22,10 +22,30 @@ app.MapPatch("/person", (Person person) => $"Hello {person.FirstName} {person.La
 app.MapDelete("/person/{id}", (int id) => $"Hello person with id={id} is deleted!");
 
 //Practice examples of HTTP request verbs
+/*
+ * Use IResult to return more descriptive results from API.
+ * In general, you can return following from the endpoint
+ * 1) void -> returns 200 status code with empty body
+ * 2) string -> returns 200 status code with HTTP response content-type as text/pain
+ * 3) T -> returns 200 status code with object T serialized as JSON object with HTTP response content-type as application/json
+ * 4) IResult -> Manually send status codes with methods provided by their implementations. (Result & TypedResult). This is used to send mode descriptive status codes like NON-happy paths.
+ * Result & TypedResult are static helper classes that provides static methods to return different status codes as per requirements. 
+ */
+app.MapGet("/fruit", () => FruitHandler.GetFruits().Any() ? Results.Ok(FruitHandler.GetFruits()) : Results.NotFound());
+app.MapGet("/fruit/{id}", (int id) => FruitHandler.GetFruits().Any(x => x.Id == id) ? Results.Ok(FruitHandler.GetFruit(id)) : Results.NotFound());
 app.MapPost("/fruit", (Fruit fruit) => FruitHandler.AddFruit(fruit));
-app.MapGet("/fruit", () => FruitHandler.GetFruits());
-app.MapGet("/fruit/{id}", (int id) => FruitHandler.GetFruit(id));
-app.MapDelete("/fruit/{id}", (int id) => FruitHandler.RemoveFruit(id));
+app.MapDelete("/fruit/{id}", (int id) =>
+{
+    if (FruitHandler.GetFruits().Any(x => x.Id == id))
+    {
+        FruitHandler.RemoveFruit(id);
+        Results.Ok();
+    }
+    else
+    {
+        Results.NotFound();
+    }
+});
 
 
 app.Run();
