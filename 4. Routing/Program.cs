662d2582c5fa0e83e3917286a5a71e177e1ab92c
craftycoder07 <22353899+craftycoder07@users.noreply.github.com>
2025-07-01 18:09:30 -0400

    Intro to Endpoint Routing in ASP.NET

---
+++ 4. Routing/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+/* ************************
+ * WHAT IS ROUTING IN .NET?
+ * Routing is process of selecting which endpoints to execute based on http request received. Routing is very powerful in .NET Core.
+ */
+
+/* ************************
+ * WHY ROUTING MIDDLEWARE WAS CREATED (SEPARATED)?
+ * In .NET Core 2.0 & 2.1 routing was part of Razor pages and MVC framework.
+ * Hence, other parts of the program were not able to use routing. (e.g. Authorization middleware now can use information stored by routing middleware on 'HttpContext' to figure out if use is authorized to execute that endpoint)
+ * Since then routing is separated into a middleware called 'Routing Middleware'. It is automatically added if not directly mentioned in the code when using endpoints.
+ */
+
+/* ************************
+ * HOW ROUTING WORKS IN .NET?
+ * Whenever http request comes to .NET app, it passes through routing middleware. This middleware maintains list of all the endpoints with its 'PATH' called 'Route Template'
+ * If http request 'PATH' matches with list of paths available, this middleware updates 'HttpContext' object with which endpoint to execute.
+ * Then request moves to next middleware until it reaches to endpoint middleware where the actual handler is executed.
+ */
+
+app.UseRouting();
+
+/* ************************
+ * SIMPLE ROUTE TEMPLATE SYNTAX
+ * Route Templates consists of 'Segments'.
+ * Route Templates is divided into segments with separators. You can use any separator but '/' is a standard.
+ * There are two types of segments. 'Literal' or 'Parameter'.
+ * 'Literal segment' is constant (case insensitive) where 'Parameter Segment' is variable.
+ * In following example 'product' is literal while 'category' and 'name' are 'Parameter' (required) segment
+ */
+app.MapGet("/product/{category}/{name}", () => "Hello World!");
+
+app.Run();
